32982f9db8326c7dcdf5c9c0c4386143536f8912
Frank <fsolleveld@outlook.com>
2021-08-21 21:55:14 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Frank Solleveld on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

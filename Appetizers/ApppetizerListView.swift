fae51679b3094f4c1d10238e0e59e5345b0c6b5f
Frank <fsolleveld@outlook.com>
2021-08-22 10:46:58 +0200

    added list cells

--- Appetizers/ApppetizerListView.swift
+++ Appetizers/ApppetizerListView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct ApppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizer List View")
-                .navigationTitle(Text("🥨 Appetizers"))
+            List(MockData.appetizers) { appetizer in
+                HStack {
+                    Image("asian-flank-steak")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(10)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        Text("€\(appetizer.price, specifier: "%.2f")")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.leading)
+                }
+            }
+            .navigationTitle(Text("🥨 Appetizers"))
         }
     }
 }

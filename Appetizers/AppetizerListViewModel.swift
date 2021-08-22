64a39f25ef6882d0692b5a0384d4469d26bf1470
Frank <fsolleveld@outlook.com>
2021-08-22 11:46:03 +0200

    refactored network call to be in a model

---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AppetizerListViewModel.swift
+//  AppetizerListViewModel
+//
+//  Created by Frank Solleveld on 22/08/2021.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published var appetizers: [Appetizer] = []
+
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}

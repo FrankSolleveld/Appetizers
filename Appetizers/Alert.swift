4516f7f209e867be8cd8ff0ee84dcb16a1f9f4fa
Frank <fsolleveld@outlook.com>
2021-08-23 14:46:25 +0200

    added alerts

---
+++ Appetizers/Alert.swift
@@ -0,0 +1,37 @@
+//
+//  Alert.swift
+//  Alert
+//
+//  Created by Frank Solleveld on 23/08/2021.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidData          = AlertItem(
+                                            title: Text("Server Error"),
+                                            message: Text("The data received from the server was invalid."),
+                                            dismissButton: .default(Text("OK")))
+
+    static let invalidResponse      = AlertItem(
+                                            title: Text("Server Error"),
+                                            message: Text("Invalid response from the server."),
+                                            dismissButton: .default(Text("OK")))
+
+    static let invalidURL           = AlertItem(
+                                            title: Text("Server Error"),
+                                            message: Text("There was an issue connecting to the server."),
+                                            dismissButton: .default(Text("OK")))
+
+    static let unableToComplete     = AlertItem(
+                                            title: Text("Server Error"),
+                                            message: Text("Unable to complete your request."),
+                                            dismissButton: .default(Text("OK")))
+}

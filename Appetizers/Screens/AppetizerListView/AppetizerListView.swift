030ebe29730af6699a9cc45a234bfdca59fa1a8c
Frank <fsolleveld@outlook.com>
2021-08-23 20:14:56 +0200

    detail view works with selected appetizer

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,18 +10,32 @@ import SwiftUI
 struct AppetizerListView: View {
 
     @StateObject var viewModel = AppetizerListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
 
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            isShowingDetail = true
+                            selectedAppetizer = appetizer
+                        }
                 }
                 .navigationTitle(Text("🥨 Appetizers"))
+                .disabled(isShowingDetail)
             }
             .onAppear { viewModel.getAppetizers() }
+            .blur(radius: isShowingDetail ? 20 : 0)
             .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
+
+            if isShowingDetail {
+                AppetizerDetailView(
+                    appetizer: selectedAppetizer!,
+                    isShowingDetail: $isShowingDetail)
             }
 
             if viewModel.isLoading {
